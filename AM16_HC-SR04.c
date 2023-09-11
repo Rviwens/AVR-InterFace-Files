ae71b5a6dacd0ef6dfcbb744f28c551c0011a640
Rviwens <142454502+Rviwens@users.noreply.github.com>
2023-09-10 20:52:10 -0700

    Add files via upload

---
+++ AM16_HC-SR04.c
@@ -0,0 +1,103 @@
+#define F_CPU 20000000UL
+#define Trigger_pin PINA0
+
+
+int TimerOverflow = 0;
+
+void SendPulse(void){
+	PORTA |= (1 << Trigger_pin);
+	_delay_us(15);
+	PORTA &= (~(1 << Trigger_pin));
+}
+
+
+void HC_SR04_Init(){
+	// Set GPIO registers
+	DDRA = 0x01;		/* Make trigger pin as output */
+	PORTD = 0xFF;
+
+	//Set Timer 1 to use Input Capture Pin and keep time
+}
+
+//Declare Variables
+short ErrorFlag =0;
+long count;
+long double distance;
+long long ErrorCount = 0;
+
+double HC_SR04_Distance(){
+	HC_SR04_Init();
+	TIMSK |= (1 << TOIE1);	/* Enable Timer1 overflow interrupts */
+	TCCR1A = 0;
+	SendPulse();
+	_delay_us(375);
+	/* Wait for rising edge */
+	while ((TIFR & (1 << ICF1)) == 0)
+	{
+		ErrorCount++;
+		if(ErrorCount>1000000){
+			Error(0);
+			ErrorFlag=1;
+			break;
+		}
+	};
+	if(ErrorFlag==1){return 0.0000;}
+
+	//set Timer 1 to start counting the time for signal to be received
+	TCNT1 = 0;	/* Clear Timer counter */
+	TCCR1B = 0x01;	/* Capture on falling edge, No prescaler */
+	TIFR = 1<<ICF1;	/* Clear ICP flag (Input Capture flag) */
+	TIFR = 1<<TOV1;	/* Clear Timer Overflow flag */
+	TimerOverflow = 0;/* Clear Timer overflow count */
+	ErrorCount=0;
+
+
+	/* Wait for falling edge */
+
+	while ((TIFR & (1 << ICF1)) == 0)
+	{
+		ErrorCount++;
+		if(ErrorCount>1000000){
+			Error(1);
+			ErrorFlag=1;
+			break;
+		}
+	}
+	ErrorCount=0;
+	if(ErrorFlag==1){return 0.0000;}
+	//Caculate Distance
+
+	count = ICR1 + (65535 * TimerOverflow);	/* Take count */
+	/* 20MHz Timer freq, sound speed =343 m/s */
+	distance = (double)count * 0.0008575;
+	char testH[20];
+	USART_Send("\r\n\nTesting Distance=||||| ");
+	dtoa(distance,testH);
+	USART_Send(testH);
+	USART_Send("\r\n");
+	if (ErrorFlag!=1&& distance<=400){
+		return distance;
+
+		} else{
+		return 0.0000;
+	}
+}
+
+
+ISR(TIMER1_OVF_vect)
+{
+	TimerOverflow++;	/* Increment Timer Overflow count */
+}
+
+
+
+
+void Error(uint8_t ErrorCode){
+	switch (ErrorCode)
+	{
+		case 0: USART_Send("\r\n Error At -> \"Waiting for Raising Edge \" \r\n"); break;
+		case 1: USART_Send("\r\nError At -> \"Waiting for Falling Edge \"\r\n ");break;
+		case 2: USART_Send("\r\nError At -> \"UnKnown Error \" ");break;
+	}
+	
+}
---
+++ SPI_ATM16.c
@@ -0,0 +1,166 @@
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <math.h>
+#include <string.h>
+
+
+
+
+
+#if defined(SPIMast)
+
+static int Device_Mode; 
+void SPI_MAST_Init(int dataOrder,int Prescaler,int clockPolarity, int Cphase, int DoubleTime){
+Device_Mode=0;
+DDRB&=~(1<<PB6);
+DDRB|=(1<<PB5);
+DDRB|=(1<<PB4);DDRB|=(1<<PB7);
+SPSR=(DoubleTime<<SPI2X);	
+SPCR=0x00;
+SPCR|=(dataOrder<<DORD);		
+SPCR|=(1<<SPE)|(1<<SPIE)|(clockPolarity<<CPOL)|(Cphase<<CPHA)|(1<<MSTR);
+
+switch(Prescaler){
+case 4: SPCR&=~(1<<SPR1);SPCR&=~(1<<SPR0);
+break;
+case 16:SPCR&=~(1<<SPR1);SPCR|=(1<<SPR0);
+break;
+case 64:SPCR|=(1<<SPR1);SPCR&=~(1<<SPR0);
+break;
+case 128:SPCR|=(1<<SPR1);SPCR|=(1<<SPR0);
+break;
+}
+
+}
+
+#if defined(SPITX)
+char SPI_Write(char data)		/* SPI write data function */
+{
+	//PORTB&=~(1<<SS);
+	_delay_ms(1);
+	char flush_buffer;
+	SPDR = data;			/* Write data to SPI data register */
+	while(!(SPSR & (1<<SPIF)));	/* Wait till transmission complete */
+	flush_buffer = SPDR;		/* Flush received data */
+    //PORTB|=(1<<SS);
+    return flush_buffer; 
+}
+#endif
+#if defined(SPIRX)
+char SPI_MR()				/* SPI read data function */
+{
+	SPDR = 0xFF;
+	while(!(SPSR & (1<<SPIF)));	/* Wait till reception complete */
+	return(SPDR);			/* Return received data */
+}
+#endif
+#endif
+
+
+
+
+
+
+
+
+
+
+
+#if defined(SPISlave)
+ void SPI_SLAVE_Init()
+ {
+	Device_Mode=1;
+	DDRB &= ~(1<<PB5);  /* Make MOSI, SCK, SS as input pins */
+	DDRB &= ~(1<<PB7);
+	DDRB &= ~(1<<PB4);
+	DDRB |= (1<<PB6);			/* Make MISO pin as 
+						output pin */
+	SPCR = (1<<SPE);			/* Enable SPI in slave mode */
+}
+char SPI_SR()			/* SPI Receive data function */
+{
+	while(!(SPSR & (1<<SPIF)));	/* Wait till reception complete */
+	return(SPDR);			/* Return received data */
+}
+#endif
+
+
+
+
+
+
+
+
+
+
+#if defined(SPITX)
+uint8_t SPI_transfer(uint8_t data)
+{
+	SPDR = 0xFF;
+	while(!(SPSR & (1<<SPIF)));	/* Wait till reception complete */
+	return(SPDR);
+}
+#endif
+
+
+
+
+
+// void SPI_Str(char *str){
+// //SPI_Write(" ");
+// for (int i=0; str[i]!=0; i++){
+// _delay_ms(1);
+// SPI_Write(str[i]);
+// //USART_TxChar(str[i]);
+// }
+// 
+// SPI_Write('|');	
+// }
+
+
+// void SPI_Receive(char *buffer){
+// 	memset(buffer,0,strlen(buffer));
+// 	for (int i=0; buffer[i-1]!='|';i++)
+// 	{
+// 		buffer[i]=SPI_SR();
+// 		//USART_TxChar(buffer[i]);
+// 	}// }
+
+// void SPI_Read(char *buffer){
+// 	//PORTB&=~(1<<SS);
+// 	memset(buffer,0,strlen(buffer));
+// int count =0;	
+// do{
+// buffer[count]=SPI_MR();
+// //USART_TxChar(buffer[count])		
+// _delay_ms(50);
+// count++;
+// } while(buffer[count-1]!='|');
+// }
+
+
+
+
+
+// void SPI_USART_MREAD(){
+// char temp[100]; 
+//	USART_Send("\r\n Receiving Echo: ");
+// 	 SPI_Read(temp);
+// // USART_Send(temp);
+// }
+// 
+// void SPI_USART_SREAD(){
+// char temp[100];
+// USART_Send("\r\n Receiving Echo: ");
+/*	SPI_Receive(temp);*/
+//USART_Send(temp);
+// }
+// 
+
+
+
+
