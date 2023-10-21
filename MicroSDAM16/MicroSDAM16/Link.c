e1fd8526e9173811ed7d9cc3ebb56a96d380f3dc
Rviwens <142454502+Rviwens@users.noreply.github.com>
2023-10-21 15:15:32 -0700

    Add files via upload

---
+++ MicroSDAM16/MicroSDAM16/Link.c
@@ -0,0 +1,28 @@
+#define F_CPU 20000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+#include <math.h>
+#include <string.h>
+
+char StatusBuff[511];
+
+#define USARTTX
+#define USARTRX
+#define SPITX
+#define SPIMast
+#define FAT32
+#define SDRSB
+#define SDWSB
+
+
+char token;
+
+
+
+
+
+
+
---
+++ MicroSDAM16/MicroSDAM16/main.c
@@ -0,0 +1,29 @@
+#include "Link.c"
+#include <AM16_Timer_Functions.c>
+#include <USART_AM16.c>
+#include <SPI_ATM16.c>
+#include <MicroSD_AM16.c>
+
+int main(void)
+{
+
+// memset(buff,59,512);
+
+DDRA=0xF;
+PORTA=0xF;
+
+USART_init(0,8,9600);
+SPI_MAST_Init(0,128,0,0,0);
+
+
+char SDIS = SD_Init();
+USART_Send("\r\nHello Void. I have awoken\r\n Response | ");
+USART_Int_Str(SDIS,0);
+
+ if(SDIS==5){
+FAT32_Init();
+FAT32_Open_File("VOID");
+SD_RSB(StatusBuff,0x5700576,&token);	
+return 0xFF; 	
+}
+}
