c3f60f59449dd5bed11f8244c80e18e7488aafed
Rviwens <142454502+Rviwens@users.noreply.github.com>
2024-03-21 13:14:44 -0700

    Update FAT32_SSD1360_Driver.c

--- atmega-16Library/FAT32_SSD1360_Driver.c
+++ atmega-16Library/FAT32_SSD1360_Driver.c
@@ -6,3 +6,16 @@
 #include <avr/interrupt.h>
 #include <math.h>
 #include <string.h>
+
+#define FNAME "1360CHR"
+#define StrLen 7
+
+void FindChar(uint8_t chr){
+
+uint16_t loc = (chr*7)+0x20;
+char Overflow = (int)loc/512;
+// Create A ConstMin for loc
+
+FAT32_FILE_Read_Sector_In_Cluster("FNAME",0,Overflow);	
+}
+
