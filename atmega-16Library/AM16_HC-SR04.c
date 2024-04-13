a471da0286a604ade1fbe0dd9864bd755d0753b9
Rviwens <142454502+Rviwens@users.noreply.github.com>
2024-04-12 21:00:59 -0700

    Add files via upload

--- atmega-16Library/AM16_HC-SR04.c
+++ atmega-16Library/AM16_HC-SR04.c
@@ -1,102 +1,9 @@
 
-#define Trigger_pin PINA0
 
+#define F_CPU 20000000UL
+#define  Trigger_pin	PA2	/* Trigger pin */
 
-
-
-int TimerOverflow = 0;
-
-
-
-void SendPulse(void){
-	PORTA |= (1 << Trigger_pin);
-	_delay_us(15);
-	PORTA &= (~(1 << Trigger_pin));
-}
-
-
-
-void HC_SR04_Init(){
-	// Set GPIO registers
-	DDRA = 0x01;		/* Make trigger pin as output */
-	PORTD = 0xFF;
-
-	//Set Timer 1 to use Input Capture Pin and keep time
-}
-
-
-
-//Declare Variables
-short ErrorFlag =0;
-long count;
-long double distance;
-long long ErrorCount = 0;
-
-	  
-
-
-
-double HC_SR04_Distance(){
-	HC_SR04_Init();
-	TIMSK |= (1 << TOIE1);	/* Enable Timer1 overflow interrupts */
-	TCCR1A = 0;
-	SendPulse();
-	_delay_us(375);
-	/* Wait for rising edge */
-	while ((TIFR & (1 << ICF1)) == 0)
-	{
-		ErrorCount++;
-		if(ErrorCount>1000000){
-			Error(0);
-			ErrorFlag=1;
-			break;
-		}
-	};
-	if(ErrorFlag==1){return 0.0000;}
-
-	//set Timer 1 to start counting the time for signal to be received
-	TCNT1 = 0;	/* Clear Timer counter */
-	TCCR1B = 0x01;	/* Capture on falling edge, No prescaler */
-	TIFR = 1<<ICF1;	/* Clear ICP flag (Input Capture flag) */
-	TIFR = 1<<TOV1;	/* Clear Timer Overflow flag */
-	TimerOverflow = 0;/* Clear Timer overflow count */
-	ErrorCount=0;
-
-
-	/* Wait for falling edge */
-
-	while ((TIFR & (1 << ICF1)) == 0)
-	{
-		ErrorCount++;
-		if(ErrorCount>1000000){
-			Error(1);
-			ErrorFlag=1;
-			break;
-		}
-	}
-	ErrorCount=0;
-	if(ErrorFlag==1){return 0.0000;}
-	//Caculate Distance
-
-	count = ICR1 + (65535 * TimerOverflow);	/* Take count */
-	/* 20MHz Timer freq, sound speed =343 m/s */
-	distance = (double)count * 0.0008575;
-	char testH[20];
-	USART_Send("\r\n\nTesting Distance=||||| ");
-	dtoa(distance,testH);
-	USART_Send(testH);
-	USART_Send("\r\n");
-	if (ErrorFlag!=1&& distance<=400){
-		return distance;
-
-		} else{
-		return 0.0000;
-	}
-}
-
-
-
-
+volatile int TimerOverflow = 0;
 
 
 
@@ -107,20 +14,91 @@ ISR(TIMER1_OVF_vect)
 
 
 
-
-
-
-
-
-
-
-
 void Error(uint8_t ErrorCode){
 	switch (ErrorCode)
 	{
-		case 0: USART_Send("\r\n Error At -> \"Waiting for Raising Edge \" \r\n"); break;
-		case 1: USART_Send("\r\nError At -> \"Waiting for Falling Edge \"\r\n ");break;
+		case 0: USART_Send("\r\n Error At -> \"Waiting for Raising Edge Error 0\" \r\n"); break;
+		case 1: USART_Send("\r\nError At -> \"Waiting for Falling Edge Error 1\"\r\n ");break;
 		case 2: USART_Send("\r\nError At -> \"UnKnown Error \" ");break;
 	}
+
+}
+
+short ErrorFlag =0;
+long count;
+long double distance;
+long long ErrorCount = 0;
+
+
+
+double HC_SR04_Distance(void)
+{
+	DDRA |= (1<<Trigger_pin);/* Make trigger pin as output */
+	PORTD |= (1<<PD6);
+
+	TIMSK |= (1 << TOIE1);
+	TCCR1A = 0;
 	
+	
+ErrorCount=0;
+count=0;
+distance=0;
+ErrorFlag=0; 
+	/* Give 10us trigger pulse on trig. pin to HC-SR04 */
+	PORTA |= (1 << Trigger_pin);
+	_delay_us(15);
+	PORTA &= (~(1 << Trigger_pin));
+	
+	TIMSK = (1 << TOIE1);
+	TCCR1A = 0;
+	TCNT1 = 0;	/* Clear Timer counter */
+	TCCR1B |= 0x41;	/* Capture on rising edge, No prescaler*/
+	TCCR1B |=(1<<CS10)|(1<<CS11);
+	
+	TIFR |= 1<<ICF1;	/* Clear ICP flag (Input Capture flag) */
+	TIFR |= 1<<TOV1;	/* Clear Timer Overflow flag */
+
+	/*Calculate width of Echo by Input Capture (ICP) */
+	
+	
+	while ((TIFR & (1 << ICF1)) == 0)
+	{
+		ErrorCount++;
+		if(ErrorCount>100000){
+			Error(0);
+			ErrorFlag=1;
+			break;
+		}
+	}
+	if(ErrorFlag==1)return 0;
+	
+	
+TCNT1 = 0;	/* Clear Timer counter */
+TCCR1B = 0x01;	/* Capture on falling edge, No prescaler */
+TCCR1B |=(1<<CS10)|(1<<CS11);
+	
+TIFR = 1<<ICF1;	/* Clear ICP flag (Input Capture flag) */
+TIFR = 1<<TOV1;	/* Clear Timer Overflow flag */
+
+TimerOverflow = 0;/* Clear Timer overflow count */
+
+
+while ((TIFR & (1 << ICF1)) == 0)
+{
+	ErrorCount++;
+	if(ErrorCount>100000){
+		Error(1);
+		ErrorFlag=1;
+		break;
+	}
+}
+
+
+
+if(ErrorFlag==1)return 0;
+count = ICR1 + (65535 * TimerOverflow);	/* Take count */
+/* 8MHz Timer freq, sound speed =343 m/s */
+distance = (double)(17150*(count)*0.0000032);
+TIMSK&=~(1 << TOIE1);
+return distance;
 }
